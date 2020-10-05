8d37c1c9f8a32ea4126b9fd9e175206cb9387c9e
FramesJanko <rheckman4143@gmail.com>
2020-10-04 23:27:45 -0400

    Need to work on jump drag

---
+++ FallingRocks/Assets/Player.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    
+
+    private Rigidbody2D rb;
+
+    public float jumpForce = 7;
+
+    public BoxCollider2D col;
+
+    public float moveSpeed = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        rb = GetComponent<Rigidbody2D>();
+        col = GetComponent<BoxCollider2D>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Horizontal") && Input.GetAxis("Horizontal") > 0)
+        {
+            rb.velocity = new Vector2(moveSpeed, rb.velocity.y);
+        }
+        else if (Input.GetButton("Horizontal") && Input.GetAxis("Horizontal") < 0)
+        {
+            rb.velocity = new Vector2(-1*moveSpeed, rb.velocity.y);
+        }
+        if (Input.GetButtonUp("Horizontal"))
+        {
+            rb.velocity = new Vector2(0, rb.velocity.y);
+        }
+        
+
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+}
---
+++ FallingRocks/Assets/RockLauncher.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RockLauncher : MonoBehaviour
+{
+    public GameObject rockPrefab;
+    public float respawnTime = 1f;
+    private Vector2 screenBounds;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(RockSlideWave());
+
+    }
+    private void SpawnRock()
+    {
+        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+
+        GameObject a = Instantiate(rockPrefab) as GameObject;
+        a.transform.position = new Vector2(Random.Range(-screenBounds.x, screenBounds.x), screenBounds.y * 2);
+    }
+    IEnumerator RockSlideWave()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(respawnTime);
+            SpawnRock();
+
+        }
+    }
+    
+}
