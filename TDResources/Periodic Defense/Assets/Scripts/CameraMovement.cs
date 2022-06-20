5033411671fb71ead3d34b70081231e2123d554b
FramesJanko <rheckman4143@gmail.com>
2022-06-19 20:23:08 -0400

    Adding tower opacity and wasd camera movement

--- TDResources/Periodic Defense/Assets/Scripts/CameraMovement.cs
+++ TDResources/Periodic Defense/Assets/Scripts/CameraMovement.cs
@@ -6,7 +6,8 @@ public class CameraMovement : MonoBehaviour
 {
     [SerializeField]
     private float cameraSpeed;
-
+    [SerializeField]
+    private float wasdModifier;
     private Vector3 startPosition;
 
     private Vector3 newCameraPosition;
@@ -38,6 +39,22 @@ public class CameraMovement : MonoBehaviour
 
             Camera.main.transform.position = newCameraPosition;
             startPosition = Input.mousePosition;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            Camera.main.transform.position += Vector3.forward*cameraSpeed*wasdModifier*Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            Camera.main.transform.position -= Vector3.right * cameraSpeed * wasdModifier * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            Camera.main.transform.position -= Vector3.forward * cameraSpeed * wasdModifier * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            Camera.main.transform.position += Vector3.right * cameraSpeed * wasdModifier * Time.deltaTime;
         }
     }
 }
---
+++ TDResources/Periodic Defense/Assets/TowerAppearance.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerAppearance : MonoBehaviour
+{
+    public List<Material> materials;
+
+    private void Awake()
+    {
+        
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(transform.childCount);
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            Color materialColor = transform.GetChild(i).GetComponent<Renderer>().material.color;
+            materialColor = new Color(materialColor.r, materialColor.g, materialColor.b, .5f);
+            transform.GetChild(i).GetComponent<Renderer>().material.color = materialColor;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
